696a3f797d06f8a1f7ffa6e825243729ca2926eb
Abel Granda <abelgranda@hotmail.es>
2022-04-15 19:59:18 -0500

    Primer commit

---
+++ CursoSwift/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  CursoSwift
+//
+//  Created by Abel Alexander Granda Zuñiga on 15/4/22.
+//
+//
+
+import Foundation
+
+// 1. Variables
+
+var name = "Abel"
+var edad = 36
+var populations = 8_000_000
+print("Hello, World!", name, "tienes la edad de: ", edad, populations)
